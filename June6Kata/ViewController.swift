f53c325ab69ca30dae999c5aa3f788eec48cf31e
Allen Spicer <allenspicer@gmail.com>
2016-06-06 09:50:52 -0400

    Update

--- June6Kata/ViewController.swift
+++ June6Kata/ViewController.swift
@@ -10,44 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputField: UITextField!
+    @IBOutlet weak var cOrFSwitch: UISwitch!
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        version1(5)
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
-    func input() -> String {
-        let keyboard = NSFileHandle.fileHandleWithStandardInput()
-        let inputData = keyboard.availableData
-        return NSString(data: inputData, encoding: NSUTF8StringEncoding) as! String
+        @IBAction func activateButton(sender: UIButton) {
+            if let temp = Int(inputField.text!){
+                version1(temp)}
+        
     }
 
-    func version1(a:Int) -> String{
-        
-    //accept input
 
-    //program asks which conversion to make
-    print("Hello! Please tell me: would you like me to convert to celcius or fahrenheit")
-    let inputString = input()
-        if (inputString == "celcius"){
+    func version1(a:Int){
+        
+        //accept input
+
+        //program asks which conversion to make
+        
+        //program runs given number through conversion
+        
+        //program displays converted number
+        if (cOrFSwitch.on == true){
            let b = ((a - 32) * 5 )/9
-            return String("\(b) degrees celcius")
-        }else if (inputString == "fahrenheit"){
+            outputLabel.text = String("\(b) degrees celcius")
+        }else if (cOrFSwitch.on == false){
             let b = a*9/5+32
-            return String("\(b) degrees fahrenheit")
-        }else{
-            return "Sorry - that's not a valid conversion"}
-    //program runs given number through conversion
-        
-    //program displays converted number
-    
-    
-        
-        
-        
-        
+            outputLabel.text = String("\(b) degrees fahrenheit")
+        }
+ 
     }
     
-
 }
 
